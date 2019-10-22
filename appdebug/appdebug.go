e842da923f3218232113125cd2715e455eaf5a21
Jens-Uwe Mager <jum@anubis.han.de>
2019-10-22 14:35:27 +0200

    Use normal go logging to be able to used this transparently on go112 based runtimes.

--- appdebug/appdebug.go
+++ appdebug/appdebug.go
@@ -10,17 +10,20 @@
 // debug output in a module, use it like this:
 //	var dbg appdebug.DebugVar = true
 //	dbg.Debugf(c, "Hello World")
+// The context arg is kept for now to be compatible with older go
+// appengine runtimes but is not used.
 package appdebug
 
 import (
+	"log"
+
 	"golang.org/x/net/context"
-	"google.golang.org/appengine/log"
 )
 
 type DebugVar bool
 
 func (d DebugVar) Debugf(c context.Context, format string, a ...interface{}) {
 	if d {
-		log.Debugf(c, format, a...)
+		log.Printf(format, a...)
 	}
 }
