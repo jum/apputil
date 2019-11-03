773731136f1f191e2a968357a508fff443550adb
Jens-Uwe Mager <jum@anubis.han.de>
2019-11-03 13:16:24 +0100

    Keep the old appengine/log behavior for the go111 runtime to the debug output grouped in the cloud console.

--- appdebug/appdebug.go
+++ appdebug/appdebug.go
@@ -10,20 +10,35 @@
 // debug output in a module, use it like this:
 //	var dbg appdebug.DebugVar = true
 //	dbg.Debugf(c, "Hello World")
-// The context arg is kept for now to be compatible with older go
-// appengine runtimes but is not used.
+// The context arg used when running in go111 runtime mode with the old
+// appengine/log import to enable grouping our output under the request
+// in the google cloud console log.
 package appdebug
 
 import (
 	"log"
+	"os"
+	"sync"
 
 	"golang.org/x/net/context"
+
+	aplog "google.golang.org/appengine/log"
 )
 
 type DebugVar bool
 
+var isgo111 = false
+var isgo111Check sync.Once
+
 func (d DebugVar) Debugf(c context.Context, format string, a ...interface{}) {
 	if d {
-		log.Printf(format, a...)
+		isgo111Check.Do(func() {
+			isgo111 = os.Getenv("GAE_RUNTIME") == "go111"
+		})
+		if isgo111 {
+			aplog.Debugf(c, format, a...)
+		} else {
+			log.Printf(format, a...)
+		}
 	}
 }
